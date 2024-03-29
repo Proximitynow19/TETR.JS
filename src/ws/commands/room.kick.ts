ba3841c90341284a84f62348a1580402bf453a47
SuperFire <36259493+Sup3rFire@users.noreply.github.com>
2024-03-29 15:17:36 +0700

    feat: Add room.kick

---
+++ src/ws/commands/room.kick.ts
@@ -0,0 +1,27 @@
+import WebSocketManager from "../WebSocketManager";
+
+export default async function ({ client: { room } }: WebSocketManager, { data }: any) {
+  room.id = undefined;
+  room.name = undefined;
+  room.nameSafe = undefined;
+  room.type = undefined;
+  room.owner = undefined;
+  room.creator = undefined;
+  room.state = undefined;
+  room.topic = undefined;
+  room.auto = undefined;
+  room.options = undefined;
+  room.userLimit = undefined;
+  room.autoStart = undefined;
+  room.allowAnonymous = undefined;
+  room.allowUnranked = undefined;
+  room.allowBots = undefined;
+  room.userRankLimit = undefined;
+  room.useBestRankAsLimit = undefined;
+  room.forceRequireXPToChat = undefined;
+  room.bgm = undefined;
+  room.match = undefined;
+  room.players = undefined;
+
+  room.emit("kick", data);
+}
